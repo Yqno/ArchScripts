d20246ea1f70f1c093f5eba173e8507ae055ec16
Y Q N O <79216939+Yqno@users.noreply.github.com>
2023-04-10 11:58:43 +0200

    Update Archinstall.sh

--- Archinstall.sh
+++ Archinstall.sh
@@ -26,8 +26,8 @@ fi
 # Mount the partition
 mount /dev/sda1 /mnt
 
-# Install Arch Linux and KDE, excluding games packages
-pacstrap /mnt base base-devel kde-applications kde-frameworks nano grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+# Install Arch Linux and some Other Packages
+pacstrap /mnt base base-devel kde-applications kde-frameworks nano grub networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g 
 sed -i '/games/d' /mnt/etc/pacman.conf
 
 # Generate fstab
